57c3c4b6d236980b407e51035b8f664c291d7ec0
Madcat51 <51@list.ru>
2022-02-04 15:48:56 +0300

    1

--- app/src/main/java/site/madcat/retrofit/ui/MainActivity.kt
+++ app/src/main/java/site/madcat/retrofit/ui/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
             val userName = binding.nameEditText.text.toString()
 
+            
 
             githubRepoEntity.getReposForUserAsync(
                 userName,
